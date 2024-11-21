eca9874021b3f4523cda75bbcfe0ac8c8ec2389b
lenhatquynh <nhatquynh24092001@gmail.com>
2024-11-21 11:55:48 +0700

    Feat: Update assets

--- lib/core/app_styles/app_assets.dart
+++ lib/core/app_styles/app_assets.dart
@@ -6,9 +6,177 @@ import 'package:vector_graphics/vector_graphics.dart';
 abstract class AppAssets {
   static const appIcon = 'rasters/app-icon.png';
 
-  // add square  icon
+  // add square icon
   static const addSquare = AssetBytesLoader(
     'vectors/add-square.svg',
+    packageName: 'assets',
+  );
+
+  // arrow-down icon
+  static const arrowDown = AssetBytesLoader(
+    'vectors/arrow-down.svg',
+    packageName: 'assets',
+  );
+
+  // arrow-left icon
+  static const arrowLeft = AssetBytesLoader(
+    'vectors/arrow-left.svg',
+    packageName: 'assets',
+  );
+
+  // book icon
+  static const book = AssetBytesLoader(
+    'vectors/book.svg',
+    packageName: 'assets',
+  );
+
+  // calendar icon
+  static const calendar = AssetBytesLoader(
+    'vectors/calendar.svg',
+    packageName: 'assets',
+  );
+
+  // category-1 icon
+  static const category1 = AssetBytesLoader(
+    'vectors/category-1.svg',
+    packageName: 'assets',
+  );
+
+  // category-2 icon
+  static const category2 = AssetBytesLoader(
+    'vectors/category-2.svg',
+    packageName: 'assets',
+  );
+
+  // category-3 icon
+  static const category3 = AssetBytesLoader(
+    'vectors/category-3.svg',
+    packageName: 'assets',
+  );
+
+  // chart-1 icon
+  static const chart1 = AssetBytesLoader(
+    'vectors/chart-1.svg',
+    packageName: 'assets',
+  );
+
+  // chart-2 icon
+  static const chart2 = AssetBytesLoader(
+    'vectors/chart-2.svg',
+    packageName: 'assets',
+  );
+
+  // chart-3 icon
+  static const chart3 = AssetBytesLoader(
+    'vectors/chart-3.svg',
+    packageName: 'assets',
+  );
+
+  // direct-normal icon
+  static const directNormal = AssetBytesLoader(
+    'vectors/direct-normal.svg',
+    packageName: 'assets',
+  );
+
+  // edit-1 icon
+  static const edit1 = AssetBytesLoader(
+    'vectors/edit-1.svg',
+    packageName: 'assets',
+  );
+
+  // edit-2 icon
+  static const edit2 = AssetBytesLoader(
+    'vectors/edit-2.svg',
+    packageName: 'assets',
+  );
+
+  // emoji-happy icon
+  static const emojiHappy = AssetBytesLoader(
+    'vectors/emoji-happy.svg',
+    packageName: 'assets',
+  );
+
+  // filter icon
+  static const filter = AssetBytesLoader(
+    'vectors/filter.svg',
+    packageName: 'assets',
+  );
+
+  // graph-1 icon
+  static const graph1 = AssetBytesLoader(
+    'vectors/graph-1.svg',
+    packageName: 'assets',
+  );
+
+  // graph-2 icon
+  static const graph2 = AssetBytesLoader(
+    'vectors/graph-2.svg',
+    packageName: 'assets',
+  );
+
+  // info-circle icon
+  static const infoCircle = AssetBytesLoader(
+    'vectors/info-circle.svg',
+    packageName: 'assets',
+  );
+
+  // note-1 icon
+  static const note1 = AssetBytesLoader(
+    'vectors/note-1.svg',
+    packageName: 'assets',
+  );
+
+  // note-2 icon
+  static const note2 = AssetBytesLoader(
+    'vectors/note-2.svg',
+    packageName: 'assets',
+  );
+
+  // pen-close icon
+  static const penClose = AssetBytesLoader(
+    'vectors/pen-close.svg',
+    packageName: 'assets',
+  );
+
+  // refresh-square icon
+  static const refreshSquare = AssetBytesLoader(
+    'vectors/refresh-square.svg',
+    packageName: 'assets',
+  );
+
+  // repeat-circle-1 icon
+  static const repeatCircle1 = AssetBytesLoader(
+    'vectors/repeat-circle-1.svg',
+    packageName: 'assets',
+  );
+
+  // repeat-circle-2 icon
+  static const repeatCircle2 = AssetBytesLoader(
+    'vectors/repeat-circle-2.svg',
+    packageName: 'assets',
+  );
+
+  // repeat icon
+  static const repeat = AssetBytesLoader(
+    'vectors/repeat.svg',
+    packageName: 'assets',
+  );
+
+  // search-normal icon
+  static const searchNormal = AssetBytesLoader(
+    'vectors/search-normal.svg',
+    packageName: 'assets',
+  );
+
+  // tag-cross icon
+  static const tagCross = AssetBytesLoader(
+    'vectors/tag-cross.svg',
+    packageName: 'assets',
+  );
+
+  // tick-square icon
+  static const tickSquare = AssetBytesLoader(
+    'vectors/tick-square.svg',
     packageName: 'assets',
   );
 }
